fabaad67d348e6d734f25a1fdba0cf2018816ad3
Benjamin Renz <12860358+userx14@users.noreply.github.com>
2021-10-08 06:17:00 +0200

    get files from logical tag string now working

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import sqlite3
 import re
+import argparse
+from sympy.parsing.sympy_parser import parse_expr
+from sympy.logic import boolalg
+from sympy.core import symbol
 from datetime import datetime
+from fileinput import filename
 
 sqlDB = sqlite3.connect('./datengrab.db')
 sqlCursor = sqlDB.cursor()
@@ -31,6 +36,7 @@ sqlTablesInit = {
                     UNIQUE(ParentTagIdRef, ChildTagIdRef))"""
 }
 
+
 def checkIfTableExists(tablename):
     sqlCursor.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{tablename}'")
     result = sqlCursor.fetchall()
@@ -39,6 +45,7 @@ def checkIfTableExists(tablename):
     else:
         return False
 
+
 def firstTimeInit():
     #check if all tables exist
     for (tableName,tableFields) in sqlTablesInit.items():
@@ -46,10 +53,6 @@ def firstTimeInit():
             print(f"Table {tableName} does not exist yet, adding to database...")
             sqlCursor.execute(f"CREATE TABLE {tableName} {tableFields};")
 
-def checkLegalFileName(fileName):
-    if re.match("[^A-Za-z._]", fileName):
-        print("filename format not supported")
-        exit()
 
 def newFile(fileName):
     sqlCursor.execute(f"SELECT FileId FROM files WHERE FileName='{fileName}';")
@@ -59,6 +62,7 @@ def newFile(fileName):
     else:
         sqlCursor.execute(f"INSERT INTO files (FileName) VALUES (?);", (fileName,))
 
+
 def newTag(tagName):
     tagId = sqlCursor.execute(f"SELECT TagId FROM tags WHERE TagName='{tagName}';")
     fileId = sqlCursor.fetchone()
@@ -67,6 +71,7 @@ def newTag(tagName):
     else:
         taggingTime = datetime.now()
         sqlCursor.execute(f"INSERT INTO tags (TagName, TagCreationDate) VALUES (?,?);", (tagName, taggingTime))
+    
     
 def addTagToFile(fileName, tagName):
     sqlCursor.execute(f"SELECT FileId FROM files WHERE FileName='{fileName}';")
@@ -92,7 +97,12 @@ def addTagToFile(fileName, tagName):
         print("ref already exists")
     
         
-    
+def renameTag(oldTagName, newTagName):
+    sqlCursor.execute(f"""UPDATE tags
+                          SET TagName = "{newTagName}"
+                          WHERE TagName = "{oldTagName}";""")
+
+
 def findTagsOfFile(filename):
     
     #relates the files to the tags by performing a double join with the refs table
@@ -101,23 +111,179 @@ def findTagsOfFile(filename):
     sqlCursor.execute(f"""SELECT taglist.TagName 
                           FROM 
                           (
-                          SELECT * 
-                          FROM files 
-                          INNER JOIN refs ON refs.FileIdRef = files.FileId 
-                          INNER JOIN tags On tags.TagId = refs.TagIdRef
-                          WHERE FileName='{filename}' 
+                              SELECT * 
+                              FROM files 
+                              INNER JOIN refs ON refs.FileIdRef = files.FileId 
+                              INNER JOIN tags On tags.TagId = refs.TagIdRef
+                              WHERE FileName='{filename}' 
                           ) AS taglist;""")
     
     
-    tagNames = sqlCursor.fetchall()
-    print(tagNames)
+    return [tagName[0] for tagName in sqlCursor.fetchall()]
+
+
+def _getRefIdFromTagName(tagName):
+    sqlCursor.execute(f"""SELECT TagId
+                          FROM tags
+                          WHERE TagName = "{tagName}"   
+                          """)
+    return sqlCursor.fetchone()[0]
+
+
+def _sqlSubQueryFileIdWithoutTag(TagRefId, SubQueryName):
+    subQueryAllFileIdWithoutTag =  f""" ( 
+                                            SELECT refs.FileIdRef
+                                            FROM refs
+                                            LEFT OUTER JOIN (
+                                                SELECT FileIdRef
+                                                FROM refs
+                                                WHERE refs.TagIdRef ="{TagRefId}"
+                                            ) AS refsRemove
+                                            ON refs.FileIdRef = refsRemove.FileIdRef
+                                            WHERE refsRemove.FileIdRef IS NULL
+                                            GROUP BY refs.FileIdRef 
+                                        ) AS {SubQueryName} """
+    return subQueryAllFileIdWithoutTag
+
+
+def _sqlSubQueryFileIdWithTag(TagRefId, SubQueryName):
+    subQueryAllFileIdWithTag =  f"""(
+                                        SELECT refs.FileIdRef
+                                        FROM refs
+                                        WHERE refs.TagIdRef = "{TagRefId}"
+                                        GROUP BY refs.FileIdRef
+                                    ) AS {SubQueryName} """
+    return subQueryAllFileIdWithTag
+
+
+def _sqlSubQueryFromDnfAnd(dnfExpr, SubQueryName):
+    #place first subquerry as first table
+    SymbolOrNotOperator = dnfExpr.args[0]
+    sqlQuery = " ( SELECT andSubQuery0.fileIdRef FROM "
+    if(type(SymbolOrNotOperator) == boolalg.Not):
+        tagRefId = _getRefIdFromTagName(SymbolOrNotOperator.args[0])
+        sqlQuery += _sqlSubQueryFileIdWithoutTag(tagRefId, "andSubQuery0")
+    else: #is ordinary symbol / tag
+        tagRefId = _getRefIdFromTagName(SymbolOrNotOperator)
+        sqlQuery += _sqlSubQueryFileIdWithTag(tagRefId, "andSubQuery0")
     
-def findFileWithTags(LogicalTagStatement):
-    pass
+    #inner join all following arguments
+    for argIdx in range(1, len(dnfExpr.args)):
+        SymbolOrNotOperator = dnfExpr.args[argIdx]
+        if(type(SymbolOrNotOperator) == boolalg.Not):
+            tagRefId = _getRefIdFromTagName(SymbolOrNotOperator.args[0])
+            sqlQuery += " INNER JOIN " 
+            sqlQuery += _sqlSubQueryFileIdWithoutTag(tagRefId, f"andSubQuery{argIdx}")
+            sqlQuery += f" ON andSubQuery0.fileIdRef = andSubQuery{argIdx}.fileIdRef "
+        else:
+            tagRefId = _getRefIdFromTagName(SymbolOrNotOperator)
+            sqlQuery += " INNER JOIN "
+            sqlQuery += _sqlSubQueryFileIdWithTag(tagRefId, f"andSubQuery{argIdx}")
+            sqlQuery += f" ON andSubQuery0.fileIdRef = andSubQuery{argIdx}.fileIdRef "
+    sqlQuery += f" ) AS {SubQueryName} "
+    return sqlQuery        
+    
+    
+def _sqlSubQueryFromDnfOr(dnfExpr, SubQueryName):
+    #place first subquerry as first table
+    SymbolOrAndOrNotOperator = dnfExpr.args[0]
+    sqlQuery = " ( SELECT orSubQuery0.fileIdRef FROM "
+    if(type(SymbolOrAndOrNotOperator) == boolalg.Not):
+        tagRefId = _getRefIdFromTagName(SymbolOrAndOrNotOperator.args[0])
+        sqlQuery += _sqlSubQueryFileIdWithoutTag(tagRefId, "orSubQuery0")
+    elif(type(SymbolOrAndOrNotOperator) == boolalg.And):
+        sqlQuery += _sqlSubQueryFromDnfAnd(SymbolOrAndOrNotOperator, "orSubQuery0")    
+    else: #is ordinary symbol / tag
+        tagRefId = _getRefIdFromTagName(SymbolOrAndOrNotOperator)
+        sqlQuery += _sqlSubQueryFileIdWithTag(tagRefId, "orSubQuery0")
+        
+    #outer join all following arguments
+    for argIdx in range(1, len(dnfExpr.args)):
+        SymbolOrAndOrNotOperator = dnfExpr.args[argIdx]
+        if(type(SymbolOrAndOrNotOperator) == boolalg.Not):
+            tagRefId = _getRefIdFromTagName(SymbolOrAndOrNotOperator.args[0])
+            sqlQuery += f" UNION SELECT orSubQuery{argIdx}.fileIdRef FROM "
+            sqlQuery += _sqlSubQueryFileIdWithoutTag(tagRefId, f"orSubQuery{argIdx}")
+        elif(type(SymbolOrAndOrNotOperator) == boolalg.And):
+            sqlQuery += f" UNION SELECT orSubQuery{argIdx}.fileIdRef FROM "
+            sqlQuery += _sqlSubQueryFromDnfAnd(SymbolOrAndOrNotOperator, f"orSubQuery{argIdx}")
+        else:
+            tagRefId = _getRefIdFromTagName(SymbolOrAndOrNotOperator)
+            sqlQuery += f" UNION SELECT orSubQuery{argIdx}.fileIdRef FROM "
+            sqlQuery += _sqlSubQueryFileIdWithTag(tagRefId, f"orSubQuery{argIdx}")
+    sqlQuery += f" ) AS {SubQueryName} "
+    return sqlQuery    
+    
+    
+def findFilesWithTags(LogicalTagStatement):
+    #TODO check for bad input which is a valid sympy expression
+    
+    #convert "not"-operator into readable format for sympy
+    LogicalTagStatement = LogicalTagStatement.replace("!","~")
+    
+    #support implied AND   
+    TagSplitted = re.split(r'([\w]+)', LogicalTagStatement)
+    for tagIdx in range(len(TagSplitted)):
+        #check if the current token is a tag
+        if TagSplitted[tagIdx].isspace():
+            TagSplitted[tagIdx] = " & "
+        if TagSplitted[tagIdx].strip() == "~":
+            TagSplitted[tagIdx] = " &~ "
+        
+    
+    ExpressionString = ''.join(TagSplitted)
+    print(f"expr: {ExpressionString}")
+    
+    dnfExpr = boolalg.to_dnf(parse_expr(ExpressionString))
+    
+    #if(type(dnfExpr) is boolalg.Not): TODO case for only one tag non negated
+    sqlQuery = """SELECT files.FileName
+                           FROM files
+                           INNER JOIN"""
+    selectedFileRefIds = None
+    if(type(dnfExpr) == symbol.Symbol):
+        #only search for a single tag
+        TagRefId = _getRefIdFromTagName(dnfExpr)
+        sqlQuery += _sqlSubQueryFileIdWithTag(TagRefId, "combinedFileRefSubQuery")
+    elif(type(dnfExpr) == boolalg.Not):
+        #only search for a single negated tag
+        tagName = dnfExpr.args[0]
+        currentTagRefId = _getRefIdFromTagName(tagName)
+        sqlQuery += _sqlSubQueryFileIdWithoutTag(currentTagRefId, "combinedFileRefSubQuery")
+    elif(type(dnfExpr) == boolalg.And):
+        #search nested "and" with can have "not" or "symbols" as childs
+        sqlQuery += _sqlSubQueryFromDnfAnd(dnfExpr, "combinedFileRefSubQuery")
+    elif(type(dnfExpr) == boolalg.Or):       
+        sqlQuery += _sqlSubQueryFromDnfOr(dnfExpr, "combinedFileRefSubQuery") 
+        pass
+    else:
+        raise Exception("bad search string")
+    sqlQuery += """ON files.FileId = combinedFileRefSubQuery.FileIdRef"""
+    print(sqlQuery)
+    sqlCursor.execute(sqlQuery)
+    return [filename[0] for filename in sqlCursor.fetchall()]
+
+
+
+"""
+parser = argparse.ArgumentParser(description="tag based file manager")
+parser.add_argument('-a', help="adds the following tags to...")
+parser.add_argument('-c', help="...the specified filepaths which will be copied inside the tagging directory")
+parser.add_argument('-d', help="removes the following tags from...")
+parser.add_argument('-f', help="...the specified file names inside the tagging directory")
+parser.add_argument('-s', help="search for the logically evaluated expressions of tags (supporting !,&,| and ->* for all children) (either by tagname or full hierarchical name ->software->pythonprogs or with wildcard ->computer->*)")
+parser.add_argument('-i', help="database integrity check")
+parser.add_argument('-u', help="disk usage stats and number of tracked files")
+args = parser.parse_args()
+
+"""
+
+
 
 firstTimeInit()
 newFile("testFile.txt")
 newFile("testFile2.txt")
+newFile("testFile3.txt")
 newTag("tagA")
 newTag("tagB")
 newTag("tagC")
@@ -127,7 +293,12 @@ addTagToFile("testFile.txt","tagB")
 addTagToFile("testFile2.txt","tagA")
 addTagToFile("testFile2.txt","tagC")
 addTagToFile("testFile2.txt","tagD")
+addTagToFile("testFile3.txt","tagB")
+addTagToFile("testFile3.txt","tagC")
 findTagsOfFile("testFile.txt")
 findTagsOfFile("testFile2.txt")
+#findFileWithTags("tagC")
+#findFileWithTags("tagA  tagB")
+print(findFilesWithTags("tagA | tagD"))
 
 sqlDB.commit()
